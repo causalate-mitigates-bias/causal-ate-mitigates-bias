05372399371c369853c73e7679bee7131b61976d
CAte Mitigates Bias <causalatemitigatesbias@gmail.com>
2023-11-26 10:50:51 +0530

    updates

--- sync_code.sh
+++ sync_code.sh
@@ -1,11 +1,6 @@
 ::# run this file as "cmd < sync_code.sh update"
 
-# Extract the commit message from args[1]
-commit_message="$1"
-
-echo "$1"
-
 # Add all changes, commit with the provided message, and push
 git add -A
-git commit -m $1
-git push
+git commit -m "updates"
+git push --set-upstream origin main
