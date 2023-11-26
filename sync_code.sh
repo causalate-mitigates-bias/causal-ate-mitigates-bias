fa1f0dc1fd22e43418a1a1ae438d32c8ba9468b3
CAte Mitigates Bias <causalatemitigatesbias@gmail.com>
2023-11-26 10:53:59 +0530

    updates

--- sync_code.sh
+++ sync_code.sh
@@ -1,8 +1,9 @@
-::# run this file as "cmd < sync_code.sh update"
+::# run this file as "cmd < sync_code.sh"
 
 # Add all changes, commit with the provided message, and push
 git add -A
+sleep 1
 git commit -m "updates"
-sleep 2
+sleep 1
 
 git push --set-upstream origin main
