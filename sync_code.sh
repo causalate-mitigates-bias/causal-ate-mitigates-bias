b9df3fb31176d02088dbef1dac92de4f9c04cb99
CAte Mitigates Bias <causalatemitigatesbias@gmail.com>
2023-11-26 10:45:02 +0530

    $commit_message

--- sync_code.sh
+++ sync_code.sh
@@ -1,9 +1,9 @@
 ::# run this file as "cmd < sync_code.sh update"
 
+# Extract the commit message from args[1]
+commit_message="$1"
 
-# Extract the commit message from args[0]
-commit_message="$0"
-echo "commit_message is" commit_message
+echo $1
 
 # Add all changes, commit with the provided message, and push
 git add -A
