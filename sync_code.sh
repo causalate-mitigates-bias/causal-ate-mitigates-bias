162d0a7ed369e772c0bd1c8995553eb9b91f3c4e
CAte Mitigates Bias <causalatemitigatesbias@gmail.com>
2023-12-14 11:53:52 +0530

    updates

--- sync_code.sh
+++ sync_code.sh
@@ -1,7 +1,21 @@
-# run this file as "cmd < sync_code.sh"
+# run this file as "cmd < sync_code.sh" in cmd
+# or "./sync_code.sh" on any linux based terminal.
+
+# Switch to the right profile
+
+export KEY="causalatemitigatesbias"
+
+
+git config --local user.name "CAte Mitigates Bias"
+git config --local user.email  "causalatemitigatesbias@gmail.com"
+
+# Add the correct key to the github account
+git config --add --local core.sshCommand 'ssh -i ~/.ssh/$KEY'
+
 
 # Add all changes, commit with the provided message, and push
 git add -A
 # Use single quotes and not double quote. Why?
 git commit -m "updates"
+git commit -m 'updates'
 git push --set-upstream origin main
